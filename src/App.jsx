6d6b45f47eae829418442b7a6d19bfd9630d66c0
Sanchit Padwekar <86397529+soulfaced@users.noreply.github.com>
2023-08-07 21:39:16 +0530

    added all components to app.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,30 @@
 // import { useState } from 'react'
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
+import {BrowserRouter} from 'react-router-dom'
+import {About,Contact,Experience,Feedbacks,Hero,Navbar,Tech,Works,StarCanvas} from './components'
 import './App.css'
 
 function App() {
-  // const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        3d developer project
-        </div>
-    </>
+    <BrowserRouter>
+    <div className='relative z-0 bg-primary'>
+      <div className='bg-hero-pattern bg-cover bg-no-repeat bg-center'>
+        <Navbar />
+        <Hero />
+        yo
+      </div>
+      <About />
+      <Experience />
+      <Tech />
+      <Works />
+      <Feedbacks />
+      <div className='relative z-0'>
+        <Contact />
+        <StarCanvas />
+      </div>
+    </div>
+    </BrowserRouter>
   )
 }
 
