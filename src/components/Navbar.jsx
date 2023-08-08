0ed74917383fed991efb78fb403d927ec6e8cc49
Sanchit Padwekar <86397529+soulfaced@users.noreply.github.com>
2023-08-08 23:04:16 +0530

    updated navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,15 @@
-import {useEffect,useState} from 'react';
+import {useState} from 'react';
 import { Link } from 'react-router-dom';
 
 import {styles} from "../styles";
-import { NavLink } from '../constants';
+// import { NavLink } from '../constants';
 import { logo } from '../assets';
 // import {logo,menu,close} from '../assets';
 
 
 
 const Navbar = () => {
-  const [active,setActive] = useState("")
+  const [setActive] = useState("")
   return (
     <nav
     className={`${styles.paddindX} w-full flex items-center py-5 fixed top-0 z-20 bg-primary`}
